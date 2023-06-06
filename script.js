402683f696bfa9e04f7d1238625552699c073cf5
kumaarhere <kumaarkandugula@gmail.com>
2023-06-06 19:23:09 +0530

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const speakBtn=document.querySelector("#play")
+const text=document.querySelector("#text")
+
+speakBtn.addEventListener("click",() => {
+    const speech=new SpeechSynthesisUtterance(text.value)
+    speechSynthesis.speak(speech)
+});
